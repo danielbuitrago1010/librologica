30b573789cf18095277c1e5a42b4938866eb40f8
Daniel Buitrago <danielbuitrago11@gmail.com>
2023-01-02 22:47:37 -0500

    Nombre con el que se sube respaldo

---
+++ javascript/prueba.js
@@ -0,0 +1,22 @@
+var a = 8;
+var b = 7;
+var c = 5;
+var d = 8;
+a= a+b-c+d;
+b= a+b-c+d;
+c= a+b-c+d;
+d= a+b-c+d;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+a= a+b-c+d;
+b= a+b-c+d;
+c= a+b-c+d;
+d= a+b-c+d;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+
---
+++ javascript/prueba3.js
@@ -0,0 +1,18 @@
+var a= 1;
+var b=2;
+var c=3;
+a=a+2;
+b=a+2+b;
+c=a+2+c;
+console.log(a);
+console.log(b);
+console.log(c);
+a=a/2;
+b=b/2;
+c=c/2;
+console.log(a);
+console.log(b);
+console.log(c);
+
+
+

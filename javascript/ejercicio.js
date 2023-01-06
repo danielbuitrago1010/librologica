e4bf305f28deec77703501c99ce80013f641c1b7
Daniel Buitrago <danielbuitrago11@gmail.com>
2023-01-06 17:50:54 -0500

    se agrega archivo

---
+++ javascript/ejercicio.js
@@ -0,0 +1,20 @@
+const prompt = require("prompt-sync")({sigint:true});
+var nota1;
+var nota2;
+var nota3;
+var nota4;
+var promedio;
+
+nota1 = parseFloat(prompt('Ingrese la nota 1: '));
+nota2 = parseFloat(prompt('Ingrese la nota 2: '));
+nota3 = parseFloat(prompt('Ingrese la nota 3: '));
+nota4 = parseFloat(prompt('Ingrese la nota 4: '));
+promedio = (nota1+nota2+nota3+nota4)/4;
+
+console.log("el promedio del estudiante es " + promedio);
+if (promedio >= 7) {
+    console.log("El estudiante aprobo");
+    
+}else{
+ console.log("no aprobo");   
+}

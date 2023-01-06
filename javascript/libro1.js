00532e1270d30ab6364f736720b7b098a7be8920
Daniel Buitrago <danielbuitrago11@gmail.com>
2023-01-06 17:04:40 -0500

    se agrega archivo

--- javascript/libro1.js
+++ javascript/libro1.js
@@ -1,2 +1,23 @@
 var a = (3^(2+3)-1)^(1.0/2.0);
 console.log(a);
+var b = 5/2 * 3 + 4 ^ 3 * 2 / (5+2)-2;
+console.log(b);
+
+
+var num = 3;
+var coseno = Math.cos(num);
+var seno = Math.sin(num);
+var tangente = Math.tan(num);
+var raiz = Math.sqrt(num);
+var ln = Math.log(num);
+var log10 = Math.log10(num);
+
+
+console.log(coseno);
+console.log(seno);
+console.log(tangente);
+console.log(raiz);
+console.log(ln);
+console.log(log10);
+
+
